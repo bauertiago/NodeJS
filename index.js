f99a32b672fee652e568649c742a1ac8dd31ba54
bauertiago <tiagobauer@rede.ulbra.br>
2024-09-10 13:59:35 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -6,21 +6,35 @@ let mensagem = "Hello World!";
 console.log(mensagem);
 
 // arrays
-let metas = ["Tiago", "alô"]
-console.log(metas[1] + ", " + metas[0] + "!");
+// let metas = ["Tiago", "alô"]
+// console.log(metas[1] + ", " + metas[0] + "!");
 
 // objetos
-let meta = {
-    value: 'ler um livro por mês',
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }
-}
+// let meta = {
+//     value: 'ler um livro por mês',
+//     checked: false,
+//     log: (info) => {
+//         console.log(info)
+//     }
+// }
 
-meta.value = 'não é mais ler um livro'//manipulação
-meta.log(meta.value)
+// meta.value = 'não é mais ler um livro'//manipulação
+// meta.log(meta.value)
 
 // *function // arrow function*
 //function criarMeta() {}
 //const criarMeta = () => {}
+
+
+let meta = {
+    value: 'ler um livro por mês',
+    checked: true
+}
+
+let metas = [
+    meta, {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+console.log("Posição 0 é: " + metas[0].value + ", e a posição 1 é: " + metas[1].value)
